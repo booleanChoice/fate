d205ca58570e1906400e2305a3b6dd575eaa8a2f
liujianzhao <751114500@qq.com>
2017-09-08 15:14:52 +0800

    commit

--- src/main/java/com/example/fate/controller/TestController.java
+++ src/main/java/com/example/fate/controller/TestController.java
@@ -1,5 +1,21 @@
 package com.example.fate.controller;
 
-public class TestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.fate.service.TestService;
+
+@Controller
+public class TestController {
+    
+    @Autowired
+    private TestService service;
+    
+    @RequestMapping("/")
+    public String method(Model modle) {
+        
+        return "testPage";
+    }
 }
